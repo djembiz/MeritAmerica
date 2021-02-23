ee0cc0fe2d72e86772e0542885f8edd30cc1e969
Djeme Doli <Dolidjem@gmail.com>
2021-02-23 08:52:04 -0600

    third commit - Week2 assignments

--- Stanford106AAssignments/src/week2/DrawingLines.java
+++ Stanford106AAssignments/src/week2/DrawingLines.java
@@ -1,7 +1,32 @@
 package week2;
 
-import acm.program.GraphicsProgram;
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class DrawingLines extends GraphicsProgram {
+	public void init() {
+		addMouseListeners();	
+	}
+	
+    
+	/* MOUSE METHODS */
+	
+	/* Get the coordinates of the starting point when we press the mouse button */
+public void mousePressed(MouseEvent a) {
+         x0 = a.getX();
+         y0 = a.getY();	        
+}
 
+/* Get the coordinates of the ending point while we drag the mouse */
+public void mouseDragged(MouseEvent a) {
+	x1 = a.getX();
+	y1 = a.getY();
+	line = new GLine(x0,y0, x1, y1);
+	line.setColor(Color.BLACK);
+	add(line);
+}
+private GLine line;
+private int x0, y0, x1, y1;
 }
--- Stanford106AAssignments/src/week2/RandomCircles.java
+++ Stanford106AAssignments/src/week2/RandomCircles.java
@@ -1,7 +1,45 @@
 package week2;
 
-import acm.program.GraphicsProgram;
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.awt.*;
 
 public class RandomCircles extends GraphicsProgram {
-
+	
+	public void run() {
+		
+		for (int i=0; i<10; i++) {
+		double radius = rgen.nextDouble(5.0, 50.0);
+		GOval circle = randomCircle(radius);
+		Color randomColor = circleColor.nextColor();
+		circle.setFillColor(randomColor);
+		circle.setFilled(true);
+		
+		/* Add circle on Canvas, at Random Position Coordinates */
+		
+		add(circle, randomX(), randomY());
+		}
+		
+	}
+	
+	/* **** RANDOM NUMBER GENERATORS ****** */
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private RandomGenerator circleColor = RandomGenerator.getInstance();
+	
+	/* ***** METOHD TO CREATE A CIRCLE ***** */
+	private GOval randomCircle(double radius) {
+		GOval circle = new GOval(2 * radius, 2 * radius);
+		return circle;
+	}
+	private int randomX() {
+		/* Generate a random double value that will be our X coordinate */
+		RandomGenerator X = RandomGenerator.getInstance();
+		return X.nextInt(0, getWidth());
+	}
+	private int randomY() {
+		/* Generate a random double value that will be our Y coordinate */
+		RandomGenerator Y = RandomGenerator.getInstance();
+		return Y.nextInt(0, getHeight());
+	}
 }
