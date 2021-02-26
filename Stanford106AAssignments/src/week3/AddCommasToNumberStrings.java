5d18cf14bf92c75d210e53b66cb732f941d7c5c8
Djeme Doli <Dolidjem@gmail.com>
2021-02-26 11:24:59 -0600

    Week3 assignments

--- Stanford106AAssignments/src/week3/AddCommasToNumberStrings.java
+++ Stanford106AAssignments/src/week3/AddCommasToNumberStrings.java
@@ -1,11 +1,31 @@
 package week3;
 
 import acm.program.ConsoleProgram;
+/* **** Method **** */
+
 
 public class AddCommasToNumberStrings extends ConsoleProgram {
-	
 	public void run() {
-
-	}
 	
+		     digits = readLine("Enter a numeric string: ");
+		     String newDigit = addCommasToNumericString(digits);
+		     println(newDigit);
+	 }
+	
+		
+/* ****** Instance Variables ********* */
+	private String digits = "";
+
+	/* ****** METHOD DECLARATION ***** */
+	
+private String addCommasToNumericString(String str) {
+	int len = str.length();
+	String tempDigits = "";
+	while(len > 3) {
+	int start = len - 3;
+	tempDigits = ","+ str.substring(start, len) + tempDigits;
+	len = start;	
+	}
+	return (str.substring(0, len) + tempDigits);
+ }		
 }
--- Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
+++ Stanford106AAssignments/src/week3/DeleteCharactersFromString.java
@@ -1,11 +1,24 @@
 package week3;
 
 import acm.program.ConsoleProgram;
-
 public class DeleteCharactersFromString extends ConsoleProgram {
 	
 	public void run() {
-
+		String str = readLine("Please, enter a String or a text: ");
+		println("Input a character: ");   
+		char ch = readLine().charAt(0);   
+		println("The program will now remove the character "+ch+" from the string");
+        String newString = removeCharacter(str, ch);
+        println(newString);
 	}
-	
+private String removeCharacter(String str, char ch) {
+	String newString = "";
+	for(int i=0; i<str.length(); i++) {
+		if(str.charAt(i) != ch) 
+			newString += str.charAt(i);
+	}		
+	return newString;			
+ }
 }
+
+
