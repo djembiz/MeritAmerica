77eeb96423f68ef389c149fcb2c1d2bc3e97ceee
Djeme Doli <dolidjem@gmail.com>
2021-03-09 15:44:52 -0600

    Week4, Assignment2

--- Stanford106AAssignments/src/week4/NameCounts.java
+++ Stanford106AAssignments/src/week4/NameCounts.java
@@ -1,11 +1,29 @@
 package week4;
-
+import java.util.*;
 import acm.program.ConsoleProgram;
 
 public class NameCounts extends ConsoleProgram {
 	
 	public void run() {
+		while(true) {
+		    Integer count =0;
+			String name = readLine("Enter name: ");
+			if(name.equals("")) break;
+			
+		    if(namesMap.containsKey(name)) {
+			   count = namesMap.get(name)+1;
+			   namesMap.put(name, count);
+		    }
+		    else {
+		       count+=1;
+		       namesMap.put(name, count);
+			  }
+			}
+		for(String name:namesMap.keySet()) {
+			println("\nEntry "+name +" has count " +namesMap.get(name));
+			
+		}
 
 	}
-	
+	private HashMap<String, Integer> namesMap = new HashMap <String, Integer>();
 }
